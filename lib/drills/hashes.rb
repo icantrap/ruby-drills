fcd4f461d4ef37c4088e899fca18609dc80fd87c
David Brown <DavidL.Brown@vitalsource.com>
2020-05-21 14:36:51 -0400

    Drills to hash to

---
+++ lib/drills/hashes.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+# This set of drills deals with the Hash class.
+module Drills
+  module Hashes
+    # Return an empty hash
+    def self.empty
+
+    end
+
+    # Return a hash with more than two key-value pairs
+    def self.with_data
+
+    end
+
+    # Return a hash where all of the keys are strings
+    def self.string_keys
+
+    end
+
+    # Return a hash where all of the keys are numbers
+    def self.number_keys
+
+    end
+
+    # Return a hash where all of the keys are symbols
+    def self.symbol_keys
+
+    end
+
+    # Return a hash where one of keys is a symbol and one of the keys is the same symbol stringified
+    def self.mixed_keys
+
+    end
+
+    # Return a hash where one of the keys is a non-empty array
+    def self.array_key
+
+    end
+
+    # Return a hash where one of the values is a non-empty array
+    def self.array_value
+
+    end
+
+    # Return a hash where one of the values is a non-empty hash
+    def self.hash_value
+
+    end
+
+    # Return a hash where one of the values is nil
+    def self.nil_value
+
+    end
+
+    # Given a hash, return the keys in the hash as an array
+    def self.keys(hash)
+
+    end
+
+    # Given a hash, return the values in the hash
+    def self.values(hash)
+
+    end
+
+    # Given a hash, return the number of key-value pairs in the hash
+    def self.size(hash)
+
+    end
+
+    # Given a hash, return the value for the key 'first_name'
+    def self.get_first_name(hash)
+
+    end
+
+    # Given a hash, return the value for the key 16
+    def self._16(hash)
+
+    end
+
+    # Given a hash and a key, return the value
+    def self.value_of(hash, key)
+
+    end
+
+    # Given a hash and a key, return whether the key is in the hash
+    def self.key_exists?(hash, key)
+
+    end
+
+    # Given a hash and a value, set the key 'first_name' to the value provided
+    def self.set_first_name(hash, value)
+
+    end
+
+    # Given two hashes, return whether they are the same
+    def self.equal?(lhs, rhs)
+
+    end
+
+    # Given two hashes, combine them into a single hash and return the resultant hash
+    def self.combine(hash1, hash2)
+
+    end
+  end
+end
