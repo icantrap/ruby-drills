20cfa76c346cfdc8e8b406e4a730f56a36cf9f69
Jason Phebus <jason.phebus@vitalsource.com>
2020-06-12 15:43:11 -0400

    Add Strings drills Update random string maker to remove empty string possibility

---
+++ lib/drills/strings.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+# This set of drills deals with the String class.
+module Drills
+  module Strings
+    # Return an empty string
+    def self.empty
+
+    end
+
+    # Return a string with more than two characters
+    def self.with_data
+
+    end
+
+    # Return a string of only alphabetic characters
+    def self.alpha
+
+    end
+
+    # Return a string of only numbers
+    def self.numbers
+
+    end
+
+    # Return a string with a mix of numbers, letters, and symbols (like ! not :symbol)
+    def self.mixed
+
+    end
+
+    # Given a string, return the characters in the string as an array
+    def self.arrayify(string)
+
+    end
+
+    # Given a string return how many characters are in it
+    def self.size(string)
+
+    end
+
+    # Given a string and a value, return whether the value is in the string
+    def self.value_in?(string, value)
+
+    end
+
+    # Given two strings, return whether they are the same
+    def self.equal?(string1, string2)
+
+    end
+
+    # Given two strings, combine them into a single string and return the resultant string
+    def self.combine(string1, string2)
+
+    end
+
+    # Given a string, return a string with the original string combined with the reverse
+    # of the string
+    def self.combine_with_reverse(string)
+
+    end
+
+    # Given a string of words separated by a separator, return an array of the strings
+    # without the separator. i don't like how this is phrased
+    def self.separate(string, separator)
+
+    end
+
+    # Given a string, return it in all uppercase
+    def self.uppercased(string)
+
+    end
+
+    # Given a string, return it in all lowercase
+    def self.lowercased(string)
+
+    end
+
+    # Given a string, return it with each word capitalized
+    def self.capitalized(string)
+
+    end
+
+    # Given a string, return the symbol version of the string
+    def self.symbolized(string)
+
+    end
+
+    # Given a string, return the last character of the strong
+    def self.last_char(string)
+
+    end
+
+    # Given a string, return it with the last character removed
+    def self.remove_last_char(string)
+
+    end
+  end
+end
