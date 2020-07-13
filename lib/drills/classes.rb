8fe4c657e11b4fe04814dc71d116ce4aa06618ef
Jason Phebus <jason.phebus@vitalsource.com>
2020-07-13 10:37:18 -0400

    Update classes.rb

--- lib/drills/classes.rb
+++ lib/drills/classes.rb
@@ -4,7 +4,7 @@
 module Drills
   module Classes
     # Define a class Car that accepts 4 parameters on instantiation:
-    # color, make, model, year as parameterized values. You should be
+    # color, make, model, year as keyword values. You should be
     # able to get and modify those values post instantiation
   end
 end
